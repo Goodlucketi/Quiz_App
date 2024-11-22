6038e09f3719651a4ac60f14113cf7ca2f1608ee
unknown <goodlucketim94@gmail.com>
2024-11-22 15:43:50 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return ( 
-        <footer>
-            <div className="footer p-4 bg-slate-700 text-white w-11/12 mx-auto">
+        <footer className="bg-slate-700">
+            <div className="footer p-4 text-white w-11/12 mx-auto">
                 <p>Are you an Admin? <Link to="/addquiz" className="text-gray-300 underline">Add Quiz</Link></p>
             </div>
         </footer>
